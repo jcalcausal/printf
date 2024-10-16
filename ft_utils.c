2517d0bded0e9ae4764a71e21662f09fc9e218b4
Juan Carlos Alcausa Luque <jcalcausal@gmail.com>
2024-10-16 10:25:38 +0200

    printf sin Makefile

---
+++ ft_utils.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_utils.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jalcausa <jalcausa@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/16 10:02:47 by jalcausa          #+#    #+#             */
+/*   Updated: 2024/10/16 10:19:29 by jalcausa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_putchar(char c)
+{
+	write(1, &c, 1);
+	return (1);
+}
+
+int	ft_putstr(char *str)
+{
+	int	i;
+	
+	i = 0;
+	if (str == NULL)
+		return (ft_putstr("(null)"));
+	while(str[i])
+	{
+		write(1, &str[i], 1);
+		++i;
+	}
+	return (i);
+}
+
+int	ft_putptr(unsigned long int n)
+{
+	int	res;
+
+	res = 0;
+	if (n == 0)
+		return (ft_putstr("(nil)"));
+	else
+	{
+		ft_putstr("0x");
+		res = ft_hexa(n, 'x');
+	}
+	return (res + 2);
+}
+
+int	ft_count_digits(int n)
+{
+	int	i;
+	
+	i = 0;
+	if (n < 0)
+	{
+		n = -n;
+		++i;
+	}
+	while (n >= 10)
+	{
+		n = n / 10;
+		++i;
+	}
+	return (i + 1);
+}
+
+int	ft_putnbr(int n)
+{
+	if (n == -2147483648)
+		write(1, "-2147483648", 11);
+	else if (n < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr(-n);
+	}
+	else if (n > 9)
+	{
+		ft_putnbr(n / 10);
+		ft_putnbr(n % 10);
+	}
+	else
+		ft_putchar(n + '0');
+	return (ft_count_digits(n));
+}
---
+++ ft_utils_2.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_utils_2.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jalcausa <jalcausa@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/16 10:17:35 by jalcausa          #+#    #+#             */
+/*   Updated: 2024/10/16 10:24:51 by jalcausa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_count_digits_unsigned(unsigned int n)
+{
+	int	i;
+	
+	i = 0;
+	while (n >= 10)
+	{
+		n = n / 10;
+		++i;
+	}
+	return (i + 1);
+}
+
+int	ft_putnbr_unsigned(unsigned int n)
+{
+	if (n > 9)
+	{
+		ft_putnbr_unsigned(n / 10);
+		ft_putnbr_unsigned(n % 10);
+	}
+	else
+		ft_putchar(n + '0');
+	return (ft_count_digits_unsigned(n));
+}
+
+int	ft_puthex(unsigned long int n, char c)
+{
+	char	*alphabet;
+	int		res;
+	
+	res = 1;
+	if (c == 'x')
+		alphabet = "0123456789abcdef";
+	else if (c == 'X')
+		alphabet = "0123456789ABCDEF";
+	if (n > 15)
+	{
+		res = res + ft_puthexa(n / 16, c);
+		write(1, &alphabet[n % 16], 1);
+	}
+	else
+	{
+		write(1, &alphabet[n], 1);
+	}
+	return (res);
+}
